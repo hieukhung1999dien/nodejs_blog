65d454eed0af1e183d703074b23557af9ff32c99
HieuNT103 <hieulinh577@gmail.com>
2021-09-03 03:46:06 -0700

    Init commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// app.get('/', function(req, res) {
+//   return res.send('Hello World!')
+// })
+
+//arrow funtion
+app.get('/hieu', (req, res) => {
+   res.send('Hello World!')
+})
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
